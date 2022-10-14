338584b2e39d86f9ebe39bc1ab9c50e478395f7a
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-10-14 17:59:18 +0000

    Updated some parts of code

--- month.py
+++ month.py
@@ -7,38 +7,38 @@
 
 def main():
     # user input for month
-    months = input("Enter a number that represents a month (1 = January): ")
+    months = int(input("Enter a number that represents a month (1 = January): "))
     print("")
 
     # Checks the case for the correct month
     match months:
-        case "1":
+        case 1:
             print("1 = January!")
-        case "2":
+        case 2:
             print("2 = February!")
-        case "3":
+        case 3:
             print("3 = March")
-        case "4":
+        case 4:
             print("4 = April")
-        case "5":
+        case 5:
             print("5 = May")
-        case "6":
+        case 6:
             print("6 = June!")
-        case "7":
+        case 7:
             print("7 = July!")
-        case "8":
+        case 8:
             print("8 = August")
-        case "9":
+        case 9:
             print("9 = September")
-        case "10":
+        case 10:
             print("10 = October")
-        case "11":
+        case 11:
             print("11 = November")
-        case "12":
+        case 12:
             print("12 = December")
         # Checker to make sure the code marks if something incorrect is inputted
         case _:
-            print("Invalid input, using numbers from 1-12")
+            print(f"Invalid input {months}, using numbers from 1-12")
 
 
 if __name__ == "__main__":
